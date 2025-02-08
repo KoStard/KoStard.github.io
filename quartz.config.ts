6c84c92b7cdb0791ad0becbb972b816070a944ad
KoStard <rub099999720@gmail.com>
2025-02-08 20:57:24 +0100

    Quartz sync: Feb 8, 2025, 8:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Ruben's homepage",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
